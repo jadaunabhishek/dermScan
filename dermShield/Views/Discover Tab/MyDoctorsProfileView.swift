ffa200a9b836d8d7ab6dd289f92a5ded1817d866
Abhishek Jadaun <am3239@srmist.edu.in>
2024-04-09 00:35:36 +0530

    Modified Code

---
+++ dermShield/Views/Discover Tab/MyDoctorsProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MyDoctorsProfileView.swift
+//  dermScan - Patients
+//
+//  Created by Abhishek Jadaun on 08/04/24.
+//
+
+import SwiftUI
+
+struct MyDoctorsProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyDoctorsProfileView()
+}
