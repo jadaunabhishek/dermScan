edc631da89f4a85aa10973b1dcd2a313ff92639f
Abhishek Jadaun <am3239@srmist.edu.in>
2024-04-09 00:32:48 +0530

    Modified commit

---
+++ dermShield/Views/Discover Tab/PatientDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  PatientDetailsView.swift
+//  dermScan - Doctors
+//
+//  Created by Abhishek Jadaun on 06/04/24.
+//
+
+import SwiftUI
+
+struct PatientDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PatientDetailsView()
+}
